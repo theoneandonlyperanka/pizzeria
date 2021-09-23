d9e5d5139555eabe1bdfd07cd7fbce100e95bea1
theoneandonlyperanka <81999717+theoneandonlyperanka@users.noreply.github.com>
2021-09-22 20:20:01 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ menu_list_array.push(item)
 menu_list_array.sort();
 htmldata="<section class= 'cards'>"
 for(i=0;i<menu_list_array.length;i++){
-    htmldata=htmldata+'<div class="card'+'<img src="images/pizzaImg.png"/>'+menu_list_array[i]+'</div>'
+    htmldata=htmldata+'<div class="card'+'<img src="pizzaImg.png"/>'+menu_list_array[i]+'</div>'
 }
 htmldata=htmldata+"</section>"
 document.getElementById("display_addedmenu").innerHTML=htmldata;
